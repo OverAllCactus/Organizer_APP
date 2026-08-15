59c254df1f1631b26dbc3b64adb5dea71b38a3bf
OverAllCactus <131990376+OverAllCactus@users.noreply.github.com>
2026-08-15 18:34:27 +0300

    init modules

---
+++ modules/__init__.py
@@ -0,0 +1,5 @@
+from .units import UnitsConverterWidget
+from .calculator import CalculatorWidget
+from .currency import CurrencyConverterWidget
+
+__all__ = ["UnitsConverterWidget", "CalculatorWidget", "CurrencyConverterWidget"]
